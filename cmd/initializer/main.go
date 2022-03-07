5eb95ea52f4a7eee6326020cabc0e287698f5d53
Tyler Lloyd <tyler.lloyd@microsoft.com>
2022-03-07 09:17:55 -0500

    reorganize initializer main

--- cmd/initializer/main.go
+++ cmd/initializer/main.go
@@ -15,84 +15,48 @@ import (
 	"k8s.io/klog/v2"
 )
 
-func main() {
-	var (
-		wireguardInterfaceName string
-		listenPort             int
-		nodeIP                 string
-		overlayCIDR            string
-		podCIDR                string
-	)
-	flag.StringVar(&wireguardInterfaceName, "wg-dev", "wg0", "the name of the wireguard interface on the host")
-	flag.IntVar(&listenPort, "listen-port", 51820, "the listening port of the wireguard interface")
-	flag.StringVar(&nodeIP, "node-ip", "", "IP of the kubernetes node")
-	flag.StringVar(&overlayCIDR, "overlay-cidr", "100.64.0.0/16", "address space of the node overlay")
-	flag.StringVar(&podCIDR, "pod-cidr", "10.244.0.0/16", "address space of the pods")
-	flag.Parse()
-	c, err := wgctrl.New()
-	if err != nil {
-		klog.Fatalf("failed to setup wgctrl client: %s", err)
-	}
+const EmptyWireGuardKey = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="
 
-	dev, err := c.Device(wireguardInterfaceName)
-	if err == nil {
-		if dev.PublicKey.String() == "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=" {
-			klog.Warningf("%s found with empty public key, regenerating", wireguardInterfaceName)
-		} else {
-			klog.Infof("wireguard %s device configured with public key %s", dev.Name, dev.PublicKey.String())
-			os.Exit(0)
+// ensureRoutes takes destinations in the form of IP or CIDR and will
+// add them as destination routes on the provided link device.
+func ensureRoutes(link netlink.Link, destinations ...string) error {
+	for _, dst := range destinations {
+		klog.Infof("ensuring route for %s", dst)
+		var dstIPNet *net.IPNet
+		if ip := net.ParseIP(dst); ip != nil {
+			dstIPNet = toIPNet(ip)
 		}
-	}
 
-	if err != nil && !errors.Is(err, os.ErrNotExist) {
-		klog.Fatalf("failed to check device %s", wireguardInterfaceName)
-	}
+		if _, ipNet, err := net.ParseCIDR(dst); err == nil {
+			dstIPNet = ipNet
+		}
 
-	klog.Infof("wireguard device %s missing, setting up", wireguardInterfaceName)
-	wgIP := overlay.OverlayIP(nodeIP, overlayCIDR)
-
-	linkDev, err := setupWireGuardLinkDevice(wgIP, wireguardInterfaceName)
-	if err != nil {
-		klog.Fatalf("failed to setup new wireguard link device: %s", err)
-	}
-
-	cfg, err := newWireGuardConfig(wireguardInterfaceName, listenPort)
-	if err != nil {
-		klog.Fatalf("failed to create new wg config: %s", err)
-	}
-
-	if err = c.ConfigureDevice(wireguardInterfaceName, *cfg); err != nil {
-		klog.Fatalf("failed to configure device %s: %s", wireguardInterfaceName, err)
-	}
-
-	if err = netlink.LinkSetUp(linkDev); err != nil {
-		klog.Fatalf("failed to set link %s up: %s", wireguardInterfaceName, err)
-	}
-
-	if err = ensureRoutes(linkDev, overlayCIDR, podCIDR); err != nil {
-		klog.Fatalf("failed to add routes: %s", err)
-	}
-	klog.Infof("%s dev configured", wireguardInterfaceName)
-}
-
-func ensureRoutes(link netlink.Link, routes ...string) error {
-	for _, route := range routes {
-		_, ipNet, err := net.ParseCIDR(route)
+		err := ensureRoute(link, dstIPNet)
 		if err != nil {
-			return fmt.Errorf("failed to parse route %s: %w", route, err)
-		}
-		r := netlink.Route{
-			LinkIndex: link.Attrs().Index,
-			Dst:       ipNet,
-		}
-		err = netlink.RouteAdd(&r)
-		if err != nil && !errors.Is(err, os.ErrExist) {
-			return fmt.Errorf("failed to add route %s: %w", route, err)
+			return fmt.Errorf("ensureRoute failed for %s: %w", dst, err)
 		}
 	}
 	return nil
 }
 
+// ensureRoute will attempt to add the destination to the link device if it doesn't exist.
+// if the route already exists then this is a no-op. An error is returned for all other errors.
+func ensureRoute(link netlink.Link, destination *net.IPNet) error {
+	r := netlink.Route{
+		LinkIndex: link.Attrs().Index,
+		Dst:       destination,
+	}
+
+	err := netlink.RouteAdd(&r)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		return fmt.Errorf("failed to add route %q: %w", destination, err)
+	}
+	return nil
+}
+
+// setupWireGuardLinkDevice ensures the device has an interface of type `wireguard`
+// and adds the wireguardIP to the device. On Linux this is equivalent to
+// `ip link add dev $ifName type wireguard && ip address add dev $ifName $wireguardIP`.
 func setupWireGuardLinkDevice(wireguardIP, ifName string) (netlink.Link, error) {
 	la := netlink.LinkAttrs{
 		TxQLen: -1, // default from NewAttrs()
@@ -118,7 +82,8 @@ func setupWireGuardLinkDevice(wireguardIP, ifName string) (netlink.Link, error) 
 	return wg, nil
 }
 
-func newWireGuardConfig(wireguardInterfaceName string, port int) (*wgtypes.Config, error) {
+// newWireGuardConfig generates a new WireGuard host configuration with a private key and port
+func newWireGuardConfig(port int) (*wgtypes.Config, error) {
 	privateKey, err := wgtypes.GeneratePrivateKey()
 	if err != nil {
 		return nil, fmt.Errorf("failed to generate private key: %w", err)
@@ -129,14 +94,77 @@ func newWireGuardConfig(wireguardInterfaceName string, port int) (*wgtypes.Confi
 	}, nil
 }
 
+// toIPNet converts an IP to a net.IPNet (CIDR) with the longest possible prefix length
+// depending on the IP version, /32 for IPv4 and /128 for IPv6
 func toIPNet(ip net.IP) *net.IPNet {
 	ipNet := &net.IPNet{
 		IP: ip,
 	}
 	if ip.To4() != nil {
-		ipNet.Mask = net.CIDRMask(32, 32)
+		ipNet.Mask = net.CIDRMask(8*net.IPv4len, 8*net.IPv4len)
 	} else {
-		ipNet.Mask = net.CIDRMask(128, 128)
+		ipNet.Mask = net.CIDRMask(8*net.IPv6len, 8*net.IPv6len)
 	}
 	return ipNet
+}
+
+func main() {
+	var (
+		wgDeviceName string
+		listenPort   int
+		nodeIP       string
+		overlayCIDR  string
+		podCIDR      string
+	)
+	flag.StringVar(&wgDeviceName, "wg-dev", "wg0", "the name of the WireGuard interface on the host")
+	flag.IntVar(&listenPort, "listen-port", 51820, "the listening port of the WireGuard interface")
+	flag.StringVar(&nodeIP, "node-ip", "", "IP of the kubernetes node")
+	flag.StringVar(&overlayCIDR, "overlay-cidr", "100.64.0.0/16", "address space of the node overlay")
+	flag.StringVar(&podCIDR, "pod-cidr", "10.244.0.0/16", "address space of the pods")
+	flag.Parse()
+
+	c, err := wgctrl.New()
+	if err != nil {
+		klog.Fatalf("failed to setup wgctrl client: %s", err)
+	}
+
+	dev, err := c.Device(wgDeviceName)
+	if err == nil {
+		if dev.PublicKey.String() == EmptyWireGuardKey {
+			klog.Warningf("%s found with empty public key, regenerating", wgDeviceName)
+		} else {
+			klog.Infof("WireGuard %s device configured with public key %s", dev.Name, dev.PublicKey.String())
+			os.Exit(0)
+		}
+	}
+
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		klog.Fatalf("failed to check device %s", wgDeviceName)
+	}
+
+	klog.Infof("WireGuard device %s missing, setting up", wgDeviceName)
+	wgIP := overlay.OverlayIP(nodeIP, overlayCIDR)
+
+	linkDev, err := setupWireGuardLinkDevice(wgIP, wgDeviceName)
+	if err != nil {
+		klog.Fatalf("failed to setup new WireGuard link device: %s", err)
+	}
+
+	cfg, err := newWireGuardConfig(listenPort)
+	if err != nil {
+		klog.Fatalf("failed to create new wg config: %s", err)
+	}
+
+	if err = c.ConfigureDevice(wgDeviceName, *cfg); err != nil {
+		klog.Fatalf("failed to configure device %s: %s", wgDeviceName, err)
+	}
+
+	if err = netlink.LinkSetUp(linkDev); err != nil {
+		klog.Fatalf("failed to set link %s up: %s", wgDeviceName, err)
+	}
+
+	if err = ensureRoutes(linkDev, overlayCIDR, podCIDR); err != nil {
+		klog.Fatalf("failed to add routes: %s", err)
+	}
+	klog.Infof("%s dev configured", wgDeviceName)
 }
